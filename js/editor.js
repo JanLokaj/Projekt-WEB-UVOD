579d7de028f90db0b3a7538f8bfccd4722ffdaf8
Jan Lokaj <lokaj_jan@oauh.cz>
2026-09-22 12:16:09 +0200

    WYSIWYG

--- js/editor.js
+++ js/editor.js
@@ -17,56 +17,61 @@ import {
 
 import 'ckeditor5/ckeditor5.css';
 
-ClassicEditor
-    .create(document.querySelector('#message'), {
+const textarea = document.querySelector('#message');
 
-        licenseKey: 'GPL',
+if (textarea) {
+    ClassicEditor
+        .create(textarea, {
+            licenseKey: 'GPL',
 
-        plugins: [
-            Essentials,
-            Paragraph,
-            Bold,
-            Italic,
-            Underline,
-            Link,
-            List,
-            BlockQuote,
-            Image,
-            ImageToolbar,
-            ImageCaption,
-            ImageStyle,
-            ImageInsert
-        ],
+            plugins: [
+                Essentials,
+                Paragraph,
+                Bold,
+                Italic,
+                Underline,
+                Link,
+                List,
+                BlockQuote,
+                Image,
+                ImageToolbar,
+                ImageCaption,
+                ImageStyle,
+                ImageInsert
+            ],
 
-        toolbar: [
-            'undo',
-            'redo',
-            '|',
-            'bold',
-            'italic',
-            'underline',
-            '|',
-            'link',
-            'bulletedList',
-            'numberedList',
-            '|',
-            'insertImage',
-            '|',
-            'blockQuote'
-        ],
-
-        image: {
             toolbar: [
-                'imageStyle:inline',
-                'imageStyle:block',
-                'imageStyle:side',
+                'undo',
+                'redo',
                 '|',
-                'toggleImageCaption',
-                'imageTextAlternative'
-            ]
-        }
+                'bold',
+                'italic',
+                'underline',
+                '|',
+                'link',
+                'bulletedList',
+                'numberedList',
+                '|',
+                'insertImage',
+                '|',
+                'blockQuote'
+            ],
 
-    })
-    .catch(error => {
-        console.error(error);
-    });
+            image: {
+                toolbar: [
+                    'imageStyle:inline',
+                    'imageStyle:block',
+                    'imageStyle:side',
+                    '|',
+                    'toggleImageCaption',
+                    'imageTextAlternative'
+                ]
+            }
+        })
+        .then(editor => {
+            console.log('CKEditor funguje:', editor);
+        })
+        .catch(error => {
+            console.error('CKEditor chyba:', error);
+        });
+}
